9aaba667bf9218b4eb0bbce0e0a82b8f371f1af0
jialerrr <20021576@myrp.edu.sg>
2022-07-04 12:18:40 +0800

    Made scores to 100 2

--- test.php
+++ test.php
@@ -13,10 +13,11 @@
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello World!</title>
 </head>
 
 <body>
-    <title>Hello World!</title>
+
 </body>
 
 </html>
