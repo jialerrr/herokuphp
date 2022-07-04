5ee157d0b01575aed44537c01e52d20c145cac62
jialerrr <20021576@myrp.edu.sg>
2022-07-04 12:15:39 +0800

    Made all scores to 100

--- test.php
+++ test.php
@@ -7,11 +7,16 @@
 </html> -->
 
 <!-- fix : <html> element does not have a [lang] attribute -->
-<html lang="en">
-<head>
-</head>
-<body>
-  Hello World!
-</body>
-</html>
+<!DOCTYPE html>
 
+<html lang="en">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+    <title>Hello World!</title>
+</body>
+
+</html>
