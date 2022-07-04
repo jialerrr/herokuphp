1b0eed45b0e0aaa89ba9902276042d1ea8a01215
jialerrr <20021576@myrp.edu.sg>
2022-07-04 12:11:22 +0800

    Increased scores

--- test.php
+++ test.php
@@ -1,7 +1,17 @@
-<html>
+<!-- <html>
+<head>
+</head>
+<body>
+  Hello World!
+</body>
+</html> -->
+
+<!-- fix : <html> element does not have a [lang] attribute -->
+<html lang="en">
 <head>
 </head>
 <body>
   Hello World!
 </body>
 </html>
+
